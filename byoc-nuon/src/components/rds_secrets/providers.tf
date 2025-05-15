cf21032ceeb96c734cd9ddca376d5ec719a8e78b
fidiego <sayheytofred@gmail.com>
2025-05-14 21:32:00 -0500

    chore: remove unused module

--- byoc-nuon/src/components/rds_secrets/providers.tf
+++
@@ -1,7 +0,0 @@
-# this is the root account that the credentials have permissions for.
-# use it to get list of accounts and pivot to the correct one
-provider "aws" {
-  region = var.region
-}
-
-provider "kubectl" {}
