10085839785979f18ec6602ad897f13abc320438
fidiego <sayheytofred@gmail.com>
2025-05-19 23:26:40 -0500

    chore: re-enable init-db and delete pod

--- byoc-nuon/src/actions/ctl_api/init-db.sh
+++ byoc-nuon/src/actions/ctl_api/init-db.sh
@@ -15,6 +15,12 @@ echo "[ctl_api init] kubectl auth whoami"
 echo "pwd: "`pwd`
 kubectl auth whoami
 
+echo "[ctl_api init] scale up the deployment"
+kubectl scale -n ctl-api --replicas=1 deployment/ctl-api-init
+sleep 3 # so the pod has time to stand up
+
+echo "[ctl_api init] get a pod from the deployment"
+pod=`kubectl -n ctl-api get pods --selector app=ctl-api-init -o json | jq -r '.items[0].metadata.name'`
 
 echo "[ctl_api init] reading db access secrets from AWS"
 secret=`aws --region $region secretsmanager get-secret-value --secret-id=$secret_arn`
@@ -26,7 +32,7 @@ echo "[ctl_api init] preparing to initialize"
 function run_cmd() {
   echo " > cmd: $@"
   kubectl \
-    --namespace=ctl-api exec ctl-api-init -- \
+    --namespace=ctl-api exec $pod -- \
     env "PGHOST=$db_addr" "PGPORT=$db_port" "PGUSER=$admin_username" "PGPASSWORD=$admin_password" \
     psql --no-psqlrc -d "$1" -f "$2"
 }
@@ -36,24 +42,27 @@ echo "[ctl_api init] enable hstore"
 run_cmd "$admin_db" "/var/init-config/create_hstore.sql"
 sleep 5
 
-# echo "[ctl_api init] ensuring user"
-# run_cmd "$admin_db" "/var/init-config/create_user.sql"
-# sleep 5
+echo "[ctl_api init] ensuring user"
+run_cmd "$admin_db" "/var/init-config/create_user.sql"
+sleep 5
 
-# echo "[ctl_api init] grant iam to user"
-# run_cmd "$admin_db" "/var/init-config/grant_user_iam.sql"
-# sleep 5
+echo "[ctl_api init] grant iam to user"
+run_cmd "$admin_db" "/var/init-config/grant_user_iam.sql"
+sleep 5
 
-# echo "[ctl_api init] alter user to allow create db"
-# run_cmd "$admin_db" "/var/init-config/alter_user_createdb.sql"
-# sleep 5
+echo "[ctl_api init] alter user to allow create db"
+run_cmd "$admin_db" "/var/init-config/alter_user_createdb.sql"
+sleep 5
 
-# echo "[ctl_api init] create db"
-# run_cmd "$admin_db" "/var/init-config/create_db.sql"
-# sleep 5
+echo "[ctl_api init] create db"
+run_cmd "$admin_db" "/var/init-config/create_db.sql"
+sleep 5
 
-# echo "[ctl_api init] grant all on db to ctl_api"
-# run_cmd "$admin_db" "/var/init-config/grant_db.sql"
+echo "[ctl_api init] grant all on db to ctl_api"
+run_cmd "$admin_db" "/var/init-config/grant_db.sql"
 
-# echo "[ctl_api init] grant all on db to ctl_api"
-# run_cmd "$admin_db" "/var/init-config/create_hstore.sql"
+echo "[ctl_api init] grant all on db to ctl_api"
+run_cmd "$admin_db" "/var/init-config/create_hstore.sql"
+
+echo "[ctl_api init] scale down the deployment"
+kubectl scale -n ctl-api --current-replicas=1 --replicas=0 deployment/ctl-api-init
---
+++ byoc-nuon/src/components/ctl_api_init_db/templates/deployment.tpl
@@ -0,0 +1,42 @@
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: ctl-api-init
+  namespace: ctl-api
+  labels:
+    app: ctl-api-init
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: ctl-api-init
+  template:
+    metadata:
+      labels:
+        app: ctl-api-init
+    spec:
+      containers:
+        - name: ctl-api-init
+          image: "postgres:15-alpine3.20"
+          command: [ "tail", "-f", "/dev/null" ]
+          volumeMounts:
+          - name: init-config
+            mountPath: "/var/init-config"
+      volumes:
+        - name: init-config
+          configMap:
+            name: ctl-api-init
+            items:
+            - key: create_hstore.sql
+              path: create_hstore.sql
+            - key: create_user.sql
+              path: create_user.sql
+            - key: grant_user_iam.sql
+              path: grant_user_iam.sql
+            - key: alter_user_createdb.sql
+              path: alter_user_createdb.sql
+            - key: create_db.sql
+              path: create_db.sql
+            - key: grant_db.sql
+              path: grant_db.sql
