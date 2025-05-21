52b7ee119ce676339c7b4d7ee4be8ef3ea757955
fidiego <sayheytofred@gmail.com>
2025-05-21 15:41:51 -0500

    fix: better execution guarantees

--- byoc-nuon/src/actions/temporal/init.sh
+++ byoc-nuon/src/actions/temporal/init.sh
@@ -13,13 +13,6 @@ install_id="$NUON_INSTALL_ID"
 region="$REGION"
 version="1.26.2"
 cluster_name="$CLUSTER_NAME"
-role="$ROLE"
-
-
-# echo "[temporal init] updating kubeconfig"
-# aws --region $region eks update-kubeconfig --name $cluster_name --alias $cluster_name --role-arn $role
-# kubectl --v=9 config use-context $cluster_name
-# export KUBECONFIG="/root/.kube/config"
 
 echo "[temporal init] kubectl auth whoami"
 echo "pwd: "`pwd`
@@ -36,44 +29,20 @@ password=`echo $secret | jq -r '.SecretString' | jq -r '.password'`
 
 
 # TODO: re-write this so we can see the logs - maybe use exec
-# # --rm -i -t \
-function run_cmd() {
-  echo " > cmd: $1"
-  kubectl \
-    run   \
-    --restart=Never      \
-    --namespace=temporal \
-    "nuon-temporal-admintools-$(date +"%s")-$(openssl rand -hex 4)"  \
-    --image=temporalio/admin-tools:"$version" \
-    --env="SQL_HOST=$address"      \
-    --env="SQL_PORT=$port"         \
-    --env="SQL_USER=$username"     \
-    --env="SQL_PASSWORD=$password" \
-    --env="SQL_PLUGIN=postgres12"  \
-    --env="VERSION=$version"       \
-    --command \
-    -- \
-    $1
-}
-
-echo "[temporal init] creating the temporal database"
-run_cmd "temporal-sql-tool --db temporal create"
-sleep 3
-
-echo "[temporal init] setting up the temporal schema"
-run_cmd "temporal-sql-tool --db temporal setup-schema -v 0.0"
-sleep 3
-
-echo "[temporal init] updating the temporal schema"
-run_cmd "temporal-sql-tool --db temporal update-schema -d ./schema/postgresql/v12/temporal/versioned/"
-
-echo "[temporal_visibility init] creating the temporal_visibility database"
-run_cmd "temporal-sql-tool --db temporal_visibility create"
-sleep 3
-
-echo "[temporal_visibility init] setting up the temporal_visibility schema"
-run_cmd "temporal-sql-tool --db temporal_visibility setup-schema -v 0.0"
-sleep 3
-
-echo "[temporal_visibility init] updating the temporal_visibility schema"
-run_cmd "temporal-sql-tool --db temporal_visibility update-schema -d ./schema/postgresql/v12/temporal/versioned/"
+kubectl       \
+  run         \
+  -i          \
+  --rm        \
+  --tty=false \
+  --restart=Never      \
+  --namespace=temporal \
+  "nuon-temporal-admintools-$(date +"%s")-$(openssl rand -hex 4)"  \
+  --image=temporalio/admin-tools:"$version" \
+  --env="SQL_HOST=$address"      \
+  --env="SQL_PORT=$port"         \
+  --env="SQL_USER=$username"     \
+  --env="SQL_PASSWORD=$password" \
+  --env="SQL_PLUGIN=postgres12"  \
+  --env="VERSION=$version"       \
+  --command \
+  -- bash -c "temporal-sql-tool --db temporal create;  temporal-sql-tool --db temporal setup-schema -v 0.0; temporal-sql-tool --db temporal_visibility create; temporal-sql-tool --db temporal_visibility setup-schema -v 0.0; temporal-sql-tool --db temporal_visibility update-schema -d ./schema/postgresql/v12/temporal/versioned/;"
