4765bbcd05b7788e64a41c4da559fa9c3efd9810
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-05-23 01:05:53 -0500

    chore: update ctl-api image (#62)
    * feat: updated ctl-api
    
    * chore: preflight - make generic
    
    * chore: typo
    
    * chore: ctl-api: deps

--- byoc-nuon/src/actions/preflight/ensure-secrets-access.sh
+++ byoc-nuon/src/actions/preflight/ensure-secrets-access.sh
@@ -3,7 +3,7 @@
 set -u
 
 region="$REGION"
-auth0_client_secret_arn="$AUTH0_CLIENT_SECRET_ARN"
+secret_arn="$AUTH0_CLIENT_SECRET_ARN"
 
 
 aws --region $region sts get-caller-identity
@@ -11,7 +11,7 @@ aws --region $region sts get-caller-identity
 # ensure we can NOT read this specific secret
 validated=""
 echo "attempting to fetch secret: expected to fail"
-aws --region $region secretsmanager get-secret-value --secret-id="$auth0_client_secret_arn"
+aws --region $region secretsmanager get-secret-value --secret-id="$secret_arn"
 exitCode=$?
 if [ $exitCode -eq 0 ]; then
   echo "failed to fetch secret (exit code $exitCode)"
