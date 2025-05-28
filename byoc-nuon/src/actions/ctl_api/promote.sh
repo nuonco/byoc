91cc6ab2b61ff184bed8dc13c74f7aa9ddccdc3e
Jon Morehouse <jon@nuon.co>
2025-05-27 17:01:55 -0700

    temp

--- byoc-nuon/src/actions/ctl_api/promote.sh
+++ byoc-nuon/src/actions/ctl_api/promote.sh
@@ -8,6 +8,7 @@ admin_api_addr="$ADMIN_API_URL"
 
 echo "[ctl_api promote] fetching ctl-api admin api"
 kubectl get services -n ctl-api -o yaml
+nslookup admin.internal.byoc.retool.dev
 
 curl -X 'POST' \
   "$admin_api_addr/v1/general/promotion" \
