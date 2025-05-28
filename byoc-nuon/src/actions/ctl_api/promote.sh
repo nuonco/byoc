578a5e5501a4ab1f26b283e4bbab3ad6e824339a
Jon Morehouse <jon@nuon.co>
2025-05-27 17:27:50 -0700

    chore: update

--- byoc-nuon/src/actions/ctl_api/promote.sh
+++ byoc-nuon/src/actions/ctl_api/promote.sh
@@ -5,9 +5,8 @@ set -o pipefail
 set -u
 
 admin_api_addr="$ADMIN_API_URL"
-admin_api_addr="admin.internal.byoc.retool.dev"
 
 echo "[ctl_api promote] executing ctl-api promote callback"
 curl --max-time 5 -q -X 'POST' \
-  "$admin_api_addr:8082/v1/general/promotion" \
+  "$admin_api_addr/v1/general/promotion" \
   --data '{"tag":"byoc"}'
