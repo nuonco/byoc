5cc5921a030a9d3a805835840082874dfe22d6e3
fidiego <sayheytofred@gmail.com>
2025-05-29 15:20:32 -0500

    chore: refine action to use outputs and output `\du`

--- byoc-nuon/src/actions/ctl_api/init-db.sh
+++ byoc-nuon/src/actions/ctl_api/init-db.sh
@@ -4,6 +4,8 @@ set -e
 set -o pipefail
 set -u
 
+db_name="$DB_NAME" # admin db
+db_user="$DB_USER"
 db_addr="$DB_ADDR"
 db_port="$DB_PORT"
 region="$REGION"
@@ -27,7 +29,11 @@ echo "[ctl_api init] reading db access secrets from AWS"
 secret=`aws --region $region secretsmanager get-secret-value --secret-id=$secret_arn`
 admin_username=`echo $secret | jq -r '.SecretString' | jq -r '.username'`
 admin_password=`echo $secret | jq -r '.SecretString' | jq -r '.password'`
-admin_db="nuon"
+
+echo "[ctl_api init] sanity check"
+echo "these two should match"
+echo "db_user=$db_user"
+echo "username=$admin_username"
 
 echo "[ctl_api init] preparing to initialize"
 function run_cmd() {
@@ -42,33 +48,41 @@ function run_cmd() {
 
 # these can fail on subsequent runs
 echo "[ctl_api init] enable hstore"
-run_cmd "$admin_db" "/var/init-config/create_hstore.sql"
+run_cmd "$db_name" "/var/init-config/create_hstore.sql"
 sleep 5
 
 echo "[ctl_api init] ensuring user"
-run_cmd "$admin_db" "/var/init-config/create_user.sql"
+run_cmd "$db_name" "/var/init-config/create_user.sql"
 sleep 5
 
 echo "[ctl_api init] grant iam to user"
-run_cmd "$admin_db" "/var/init-config/grant_user_iam.sql"
+run_cmd "$db_name" "/var/init-config/grant_user_iam.sql"
 sleep 5
 
 echo "[ctl_api init] alter user to allow create db"
-run_cmd "$admin_db" "/var/init-config/alter_user_createdb.sql"
+run_cmd "$db_name" "/var/init-config/alter_user_createdb.sql"
 sleep 5
 
 echo "[ctl_api init] create db"
-run_cmd "$admin_db" "/var/init-config/create_db.sql"
+run_cmd "$db_name" "/var/init-config/create_db.sql"
 sleep 5
 
 echo "[ctl_api init] grant all on db to ctl_api"
-run_cmd "$admin_db" "/var/init-config/grant_db.sql"
+run_cmd "$db_name" "/var/init-config/grant_db.sql"
 
 echo "[ctl_api init] grant all on public"
 run_cmd "ctl_api" "/var/init-config/grant_public.sql"
 
 echo "[ctl_api init] hstore"
 run_cmd "ctl_api" "/var/init-config/create_hstore.sql"
+
+echo "[ctl_api init] validate"
+kubectl \
+  --namespace=ctl-api \
+  exec  -i \
+  $pod -- \
+  env "PGHOST=$db_addr" "PGPORT=$db_port" "PGUSER=$admin_username" "PGPASSWORD=$admin_password" \
+  psql --no-psqlrc -d "ctl_api" -c "\du"
 
 echo "[ctl_api init] scale down the deployment"
 kubectl scale -n ctl-api --current-replicas=1 --replicas=0 deployment/ctl-api-init
