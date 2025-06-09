44f7a4a0eebae47c328a2011dfbc1797c4aab3c1
fidiego <sayheytofred@gmail.com>
2025-06-09 14:07:50 -0500

    feat: show migrations post-deploy

---
+++ byoc-nuon/src/actions/ctl_api/migrations.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -u
+
+admin_api_addr="$ADMIN_API_URL"
+
+echo "[ctl_api promote] executing ctl-api promote callback"
+migrations=`curl -s  "$admin_api_addr/v1/general/migrations" | jq -c`
+
+# print in human legible format
+echo $migrations | jq -r '.[] | "\(.created_at) \(.status) \(.name)"'
+
+echo $migrations >> $NUON_ACTIONS_OUTPUT_FILEPATH
