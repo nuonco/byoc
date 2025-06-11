445aae856b0ec4517b5aae9c975ade849b76be27
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-06-11 12:03:05 -0500

    feat: datadog component (#148)
    * chore: rm unused inputs
    
    * feat: new component - datadog

---
+++ byoc-nuon/src/components/datadog/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.7.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.67.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.17.0"
+    }
+    utils = {
+      source  = "cloudposse/utils"
+      version = ">= 0.17.23"
+    }
+    datadog = {
+      source  = "DataDog/datadog"
+      version = "3.44.1"
+    }
+  }
+}
