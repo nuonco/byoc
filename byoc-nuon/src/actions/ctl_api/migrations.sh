b17b5d7a6da62091b7fc3dc83ae01638258bf9a7
Jordan Acosta <jordan@nuon.co>
2025-06-12 13:47:25 -0700

    fix: ctl_api_promote script failing (#155)

--- byoc-nuon/src/actions/ctl_api/migrations.sh
+++ byoc-nuon/src/actions/ctl_api/migrations.sh
@@ -6,11 +6,12 @@ set -u
 
 admin_api_addr="$ADMIN_API_URL"
 
-echo "[ctl_api migrations] executing ctl-api migrations callback"
+echo "get ctl-api migrations data"
 migrations=`curl -s  "$admin_api_addr/v1/general/migrations" | jq -c`
 
-# print in human legible format
+echo "format migrations data"
 echo $migrations | jq -r '.[] | "\(.created_at) \(.status) \(.name)"'
 outputs=`jq --null-input --argjson migrationsVar "$migrations" '{"migrations": $migrationsVar}'`
 
-echo $migrations >> $NUON_ACTIONS_OUTPUT_FILEPATH
+echo "save migrations data to outputs"
+echo $outputs >> $NUON_ACTIONS_OUTPUT_FILEPATH
