8319953744334b685aa4d69e6459a2f05c16f51c
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-06-24 15:42:03 -0500

    fix: datadog: pin versions (#178)

--- byoc-nuon/src/components/datadog/versions.tf
+++ byoc-nuon/src/components/datadog/versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.67.0"
+      version = "= 5.67.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.17.0"
+      version = "= 2.17.0"
     }
     utils = {
       source  = "cloudposse/utils"
-      version = ">= 0.17.23"
+      version = "= 0.17.23"
     }
     datadog = {
       source  = "DataDog/datadog"
