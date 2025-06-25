e422139529b437a47c26282be6187fdf7c5ef863
fidiego <sayheytofred@gmail.com>
2025-06-25 17:09:37 -0500

    chore: pin versions

--- byoc-nuon/src/components/clickhouse_cluster/versions.tf
+++ byoc-nuon/src/components/clickhouse_cluster/versions.tf
@@ -1,16 +1,18 @@
 terraform {
+  required_version = ">= 1.11.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.67.0"
+      version = "= 5.67.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.14"
+      version = "= 1.14"
     }
     utils = {
       source  = "cloudposse/utils"
-      version = ">= 0.17.23"
+      version = "= 0.17.23"
     }
   }
 }
