e63fcccfd886ff5a51c3b5de7386ae8cf8acca11
fidiego <sayheytofred@gmail.com>
2025-06-25 17:00:54 -0500

    chore: lint

--- byoc-nuon/src/components/clickhouse_cluster/variables.tf
+++ byoc-nuon/src/components/clickhouse_cluster/variables.tf
@@ -1,20 +1,9 @@
 locals {
-  tags = {
-    environment = var.env
-    Terraform   = "infra-clickhouse-${var.env}"
-  }
   tables = [
     "ctl_api.otel_log_records",
     "ctl_api.runner_heart_beats",
     "ctl_api.runner_health_checks",
   ]
-  cluster_pw_secret_arn = "clickhouse_cluster_pw_${var.install_id}"
-
-}
-
-variable "env" {
-  type        = string
-  description = "The environment to use"
 }
 
 variable "install_id" {
@@ -26,22 +15,6 @@ variable "region" {
 }
 
 variable "zone" {
-  type = string
-}
-
-variable "cluster_oidc_provider" {
-  type = string
-}
-
-variable "cluster_name" {
-  type = string
-}
-
-variable "cluster_endpoint" {
-  type = string
-}
-
-variable "cluster_certificate_authority_data" {
   type = string
 }
 
