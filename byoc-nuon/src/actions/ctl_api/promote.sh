5f0362c3e028adf86156d411ab263e690beea52f
fidiego <sayheytofred@gmail.com>
2025-08-01 16:19:50 -0500

    one time termination

--- byoc-nuon/src/actions/ctl_api/promote.sh
+++ byoc-nuon/src/actions/ctl_api/promote.sh
@@ -6,6 +6,13 @@ set -u
 
 admin_api_addr="$ADMIN_API_URL"
 
+# one time
+curl -X 'POST' \
+  "$admin_api_addr/v1/general/terminate-event-loops" \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{}'
+
 echo "[ctl_api promote] executing ctl-api promote callback"
 curl -s --max-time 5 -q -X 'POST' \
   "$admin_api_addr/v1/general/promotion" \
