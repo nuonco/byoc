62f2e49fd8f7e12c9093aa77ca78b387c9af4ce5
Jon Morehouse <jon@nuon.co>
2025-10-06 16:04:00 -0700

    chore: add action to extend runner token

---
+++ byoc-nuon/src/actions/runners/extend_token.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -e
+set -o pipefail
+set -u
+
+admin_api_url="$ADMIN_API_URL"
+runner_id="$RUNNER_ID"
+
+outputs='{}'
+
+echo "extending runner token"
+
+OUTPUT=$(curl -s \
+  --max-time 5 \
+  -q \
+  -X 'POST' \
+  "$admin_api_addr/v1/runners/$runner_id" \
+  --data '{"duration":"8760h"}')
+
+outputs=$(echo "$OUTPUT" | jq '{email: .email, expires_at: .expires_at}')
+echo "$outputs" >> "$NUON_ACTIONS_OUTPUT_FILEPATH"
