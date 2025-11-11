6505ec5f0088ba00feae333b810191e2a8870c3e
fidiego <sayheytofred@gmail.com>
2025-11-10 23:14:46 -0600

    chore: optionally restart replica

--- byoc-nuon/src/actions/clickhouse/check_and_restore_replicas.sh
+++ byoc-nuon/src/actions/clickhouse/check_and_restore_replicas.sh
@@ -8,13 +8,23 @@ echo "getting a pod"
 pod=`kubectl -n clickhouse get pods | grep installation | cut -d ' ' -f 1 | tail -n 1`
 
 echo "getting read-only tables"
-tables=`kubectl --namespace=clickhouse exec -i $pod -- clickhouse client -d ctl_api -q "SELECT table FROM system.replicas WHERE database = 'ctl_api' AND is_readonly = 1"`
+# NOTE(fd): we reverse the sort to get the runner_* tables in first.
+tables=`kubectl --namespace=clickhouse exec -i $pod -- clickhouse client -d ctl_api -q "SELECT table FROM system.replicas WHERE database = 'ctl_api' AND is_readonly = 1" | sort -rn`
 echo $tables
+
 
 # act on replicas w/ a bad status
 for table in $tables; do
     echo " > table replication in need of repair: $table"
-    kubectl --namespace=clickhouse exec -i $pod -- clickhouse client -d ctl_api -q "system restore replica on cluster simple ctl_api.$table;"
+    if [[ $RESTART == "true" ]]; then
+      echo " sql: SYSTEM RESTART REPLICA ON CLUSTER simple ctl_api.$table;"
+      kubectl --namespace=clickhouse exec -i $pod -- \
+        clickhouse client -d ctl_api --distributed_ddl_task_timeout=3600 --progress -q "SYSTEM RESTART REPLICA ON CLUSTER simple ctl_api.$table;"
+      sleep 3
+    fi
+    echo " sql: SYSTEM RESTORE REPLICA ON CLUSTER simple ctl_api.$table;"
+    kubectl --namespace=clickhouse exec -i $pod -- \
+      clickhouse client -d ctl_api --distributed_ddl_task_timeout=3600 --progress -q "SYSTEM RESTORE REPLICA ON CLUSTER simple ctl_api.$table;"
     echo " > table replication restore - taking a small nap $table"
     sleep 1
 done
