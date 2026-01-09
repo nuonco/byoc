3c32cf3a4fad4c525dcc5c95152bc70167c66525
fidiego <sayheytofred@gmail.com>
2026-01-09 17:00:07 -0600

    chore: fix action

--- byoc-nuon/src/actions/runners/update-version.sh
+++ byoc-nuon/src/actions/runners/update-version.sh
@@ -6,7 +6,7 @@ set -u
 
 admin_api_url="$ADMIN_API_URL"
 
-OUTPUTS='{}'
+RESPONSE_COUNTS='{}'
 
 echo "getting RUNNER_CONTAINER_IMAGE_TAG from ctl-api configmap"
 RUNNER_CONTAINER_IMAGE_TAG=$(kubectl get -n ctl-api configmaps ctl-api -o yaml |\
@@ -26,12 +26,22 @@ for runner_id in $runners; do
   echo "updating runner $runner_id to version $RUNNER_CONTAINER_IMAGE_TAG"
   url="$admin_api_url/v1/runners/$runner_id/settings"
 
-  response=$(curl -s --max-time 5 -X PATCH "$url" \
+  http_code=$(curl -s --max-time 5 -X PATCH "$url" \
     -H "Content-Type: application/json" \
-    -d "{\"container_image_tag\": \"$RUNNER_CONTAINER_IMAGE_TAG\"}")
+    -d "{\"container_imagjkjkjke_tag\": \"$RUNNER_CONTAINER_IMAGE_TAG\"}" \
+    -w "\n%{http_code}" \
+    -o /tmp/response_body.json)
 
-  # append runner.id and response to outputs
-  OUTPUTS=$(echo "$OUTPUTS" | jq --arg id "$runner_id" --arg resp "$response" '. + {($id): $resp}')
+  # print response as inline json
+  if jq -c . /tmp/response_body.json 2>/dev/null; then
+    :
+  else
+    cat /tmp/response_body.json
+    echo ""
+  fi
+
+  # increment counter for this status code
+  RESPONSE_COUNTS=$(echo "$RESPONSE_COUNTS" | jq --arg code "$http_code" '.[$code] = ((.[$code] // 0) + 1)')
 done
 
-echo "$OUTPUTS" >> $NUON_ACTIONS_OUTPUT_FILEPATH
+echo "{\"responses\": $RESPONSE_COUNTS}" | jq -c . >> $NUON_ACTIONS_OUTPUT_FILEPATH
