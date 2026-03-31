1cbcfcf22fa718aadaba7cff7da5d16ae41348eb
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-04-01 00:42:36 +0530

    feat: update AWS config to use shared resources

--- byoc-nuon/actions/src/clickhouse/init.sh
+++ byoc-nuon/actions/src/clickhouse/init.sh
@@ -1,11 +1,1 @@
-#!/usr/bin/env bash
-
-set -e
-set -o pipefail
-set -u
-
-echo "[clickhouse init] kubectl auth whoami"
-kubectl auth whoami -o json | jq -c
-
-# TODO: set up a standalone container to do this
-kubectl exec -n clickhouse chi-clickhouse-installation-simple-0-0-0 -- clickhouse client -q "CREATE DATABASE IF NOT EXISTS ctl_api ON CLUSTER 'simple';"
+../../../../shared/actions/src/clickhouse/init.sh
