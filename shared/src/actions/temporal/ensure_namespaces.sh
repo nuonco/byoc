a1c75f2cef9712b05307a1459f953bf47d660df3
fidiego <sayheytofred@gmail.com>
2026-05-01 10:08:26 -0500

    chore(temporal): better ensure namespaces script

--- shared/src/actions/temporal/ensure_namespaces.sh
+++ shared/src/actions/temporal/ensure_namespaces.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+# NOTE: this script runs after a temporal component and can be invoked manually.
+# it is intended to be safe to run multiple times and will explicitly set the namespace
+# retentions in order to ensure configs don't drift.
+
 set -e
 set -o pipefail
 set -u
 
-RETENTION="3d"
+DEFAULT_RETENTION="3d"
+LONG_RETENTION="30d"
 
 echo >&2 "ensuring namespaces"
-echo >&2 " > retention: $RETENTION"
+echo >&2 " > default retention: $DEFAULT_RETENTION"
+echo >&2 " > long retention:    $LONG_RETENTION"
 
 
 function create_namespace() {
@@ -15,7 +21,7 @@ function create_namespace() {
   local retention="$2"
   local description="$3"
 
-  echo >&2 " > namespace: $name"
+  echo >&2 " > namespace: $name (retention: $retention)"
   kubectl -n temporal exec -i deployment/temporal-admintools -- \
     temporal operator namespace create \
          --namespace   "$name"         \
@@ -23,13 +29,49 @@ function create_namespace() {
          --retention   "$retention"
 }
 
-declare -a namespaces=("general" "orgs" "apps" "installs" "runners" "actions" "components" "releases" "app-branches" "onboardings" "vcs", "emitters")
-echo >&2 "using $RETENTION day retention"
+function set_namespace_retention() {
+  local name="$1"
+  local retention="$2"
+
+  echo >&2 " > updating namespace retention: $name -> $retention"
+  kubectl -n temporal exec -i deployment/temporal-admintools -- \
+    temporal operator namespace update \
+         --namespace "$name"           \
+         --retention "$retention"
+}
+
+# Namespaces that use the default short retention.
+declare -a default_namespaces=(
+  "general"
+  "orgs"
+  "apps"
+  "installs"
+  "runners"
+  "actions"
+  "components"
+  "releases"
+  "app-branches"
+  "onboardings"
+)
+
+# Namespaces that need a longer retention period.
+declare -a long_namespaces=(
+  "vcs"
+  "emitters"
+)
 
 set +e
-for namespace in "${namespaces[@]}"
+for namespace in "${default_namespaces[@]}"
 do
-  echo >&2 "creating namespace $namespace"
-  create_namespace $namespace $RETENTION "$namespace for byoc nuon"
+  echo >&2 "ensuring namespace $namespace"
+  create_namespace "$namespace" "$DEFAULT_RETENTION" "$namespace for byoc nuon"
+  set_namespace_retention "$namespace" "$DEFAULT_RETENTION"
+done
+
+for namespace in "${long_namespaces[@]}"
+do
+  echo >&2 "ensuring namespace $namespace"
+  create_namespace "$namespace" "$LONG_RETENTION" "$namespace for byoc nuon"
+  set_namespace_retention "$namespace" "$LONG_RETENTION"
 done
 exit 0
