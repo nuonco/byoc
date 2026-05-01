cfe420261bf0ffde8789392b25a83743683eb3c7
fidiego <sayheytofred@gmail.com>
2026-05-01 17:05:40 -0500

    chore(ad-hoc): actions for migration

---
+++ scripts/ad-hoc/README.md
@@ -0,0 +1,3 @@
+# Actions
+
+These are actions we need to have in hand when promoting installs on from `0.19.855` to `0.19.912`
---
+++ scripts/ad-hoc/orgs-queues-enable.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -u
+
+
+api_addr="http://api.{{ .nuon.sandbox.outputs.nuon_dns.public_domain.name }}"
+admin_api_addr="http://admin.{{ .nuon.sandbox.outputs.nuon_dns.internal_domain.name }}"
+admin_email='jon@nuon.co'
+
+org_ids=$(curl -s "$admin_api_addr/v1/orgs?type=default&limit=100" -H 'accept: application/json' -H "X-Nuon-Admin-Email: $admin_email" | jq -r '.[].id')
+
+# send this payload in a patch
+# {
+#   "features": {
+#     "parallel-runner-jobs": true,
+#     "app-branches": true,
+#     "queues": true
+#   }
+# }
+
+for org_id in $org_ids; do
+  echo "Patching Settings for $org_id"
+  curl -X 'PATCH' -s -q "$admin_api_addr/v1/orgs/$org_id/admin-features" \
+    -H 'accept: application/json' \
+    -H "X-Nuon-Admin-Email: $admin_email" \
+    -H 'Content-Type: application/json' \
+    -d '{
+      "features": {
+        "parallel-runner-jobs": true,
+        "app-branches": true,
+        "queues": true
+      }
+    }'
+  sleep 5
+done
