81659649457052983fa93807dc460d995e0ffcc1
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-05-01 20:02:46 +0530

    add: misc

--- shared/actions/src/temporal/init-db.sh
+++ shared/actions/src/temporal/init-db.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "[temporal init] kubectl auth whoami"
+echo "pwd: $(pwd)"
+kubectl auth whoami -o json | jq -c
+
 echo "[temporal init] scale up the deployment"
 kubectl scale -n temporal --replicas=1 deployment/temporal-init
 kubectl wait deployment -n temporal temporal-init --for condition=Available=True --timeout=300s
 
+echo "[temporal init] get a pod from the deployment"
 pod=$(kubectl -n temporal get pods --selector app=temporal-init -o json | jq -r '.items[0].metadata.name')
 
+echo "[temporal init] preparing to initialize"
 function run_cmd() {
+  echo " > cmd: $@"
   kubectl --namespace=temporal exec -i $pod -- sh -c "$1"
 }
 
