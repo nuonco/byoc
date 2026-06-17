afccfd7b7a04e01894753d0fe1e7e50faaadda4f
FI. Diego <fidiego@users.noreply.github.com>
2026-06-17 17:14:33 -0500

    chore(ctl-api-wi): add unique id output (#783)

--- byoc-nuon-gcp/src/components/ctl_api_wi/outputs.tf
+++ byoc-nuon-gcp/src/components/ctl_api_wi/outputs.tf
@@ -6,6 +6,10 @@ output "service_account_name" {
   value = google_service_account.ctl_api.name
 }
 
+output "service_account_unique_id" {
+  value = google_service_account.ctl_api.unique_id
+}
+
 output "db_user" {
   value = trimsuffix(google_service_account.ctl_api.email, ".gserviceaccount.com")
 }
