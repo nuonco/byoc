4b47142a74bd86fabcbfbcac78a0097fb3188fb9
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-07-27 20:28:51 +0530

    chore: force destroy the cloud dns zone

--- byoc-nuon/components/management/route53_zone.tf
+++ byoc-nuon/components/management/route53_zone.tf
@@ -1,4 +1,8 @@
 # create a zone for the root domain provided by the vendor
 resource "aws_route53_zone" "root" {
   name = var.nuon_dns_domain
+
+  # Records here are written by external-dns / cert-manager, not Terraform, and
+  # a non-empty zone can't be deleted. Matches the sandbox zones.
+  force_destroy = true
 }
